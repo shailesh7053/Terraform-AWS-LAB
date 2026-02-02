7710df00e21271c155407cf0bf3519c6448a1c8b
Shailesh Kumar <108134761+shailesh7053@users.noreply.github.com>
2026-02-02 22:21:53 +0530

    Update ec2.tf

--- AWS-EC2/ec2.tf
+++ AWS-EC2/ec2.tf
@@ -1,9 +1,14 @@
 resource "aws_instance" "windows" {
-  ami                    = data.aws_ami.windows.id
-  instance_type          = "t3.micro"
-  subnet_id              = data.aws_subnets.public.ids[0]
-  vpc_security_group_ids = [aws_security_group.rdp_sg.id]
-  key_name               = "windows-key-new"
+  ami           = data.aws_ami.windows.id
+  instance_type = "t3.micro"
+
+  subnet_id = data.terraform_remote_state.vpc.outputs.public_subnet_ids[0]
+
+  vpc_security_group_ids = [
+    aws_security_group.rdp_sg.id
+  ]
+
+  key_name = aws_key_pair.windows.key_name
 
   associate_public_ip_address = true
 
