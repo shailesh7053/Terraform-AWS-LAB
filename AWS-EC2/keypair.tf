6069878f7aec8ff43aa7519ea0c21a04929400ee
Shailesh Kumar <108134761+shailesh7053@users.noreply.github.com>
2026-02-02 19:32:11 +0530

    Update keypair.tf

--- AWS-EC2/keypair.tf
+++ AWS-EC2/keypair.tf
@@ -1,9 +1,3 @@
-###resource "aws_key_pair" "windows_key" {
-  key_name   = "windows-key"
-  public_key = file("./terraform-key.pub")
-}###
-
-
 resource "tls_private_key" "windows" {
   algorithm = "RSA"
   rsa_bits  = 4096
