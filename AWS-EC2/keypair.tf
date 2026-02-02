c1356373111f9c2db036d6019c432290e1655481
Shailesh Kumar <108134761+shailesh7053@users.noreply.github.com>
2026-02-02 20:09:30 +0530

    Delete AWS-EC2/keypair.tf

--- AWS-EC2/keypair.tf
+++
@@ -1,8 +0,0 @@
-resource "tls_private_key" "windows" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-resource "aws_key_pair" "windows" {
-  key_name   = "windows-key"
-  public_key = tls_private_key.windows.public_key_openssh
-}
