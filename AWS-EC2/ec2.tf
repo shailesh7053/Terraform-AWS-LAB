76af99c3767aff97d672eea6c25bd358f0cdbabd
Shailesh Kumar <108134761+shailesh7053@users.noreply.github.com>
2026-02-02 20:06:20 +0530

    Update ec2.tf

--- AWS-EC2/ec2.tf
+++ AWS-EC2/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "windows" {
   subnet_id              = data.aws_subnets.public.ids[0]
   vpc_security_group_ids = [aws_security_group.rdp_sg.id]
 
-  key_name = aws_key_pair.windows.key_name
+  key_name = "windows-key-new"
 
   associate_public_ip_address = true
 
