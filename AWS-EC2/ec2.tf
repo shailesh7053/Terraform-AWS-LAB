bb1e9e82c1736a8e0a7bc395215dc5c698701d6e
Shailesh Kumar <108134761+shailesh7053@users.noreply.github.com>
2026-02-02 22:08:24 +0530

    Update ec2.tf

--- AWS-EC2/ec2.tf
+++ AWS-EC2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "windows" {
   ami                    = data.aws_ami.windows.id
   instance_type          = "t3.micro"
   subnet_id              = data.aws_subnets.public.ids[0]
-  vpc_security_group_ids = [data.aws_security_group.rdp_sg.id]
+  vpc_security_group_ids = [aws_security_group.rdp_sg.id]
   key_name               = "windows-key-new"
 
   associate_public_ip_address = true
