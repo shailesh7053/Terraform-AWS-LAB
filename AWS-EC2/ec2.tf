f38412a168220df504b5304647cbcf0499132460
Shailesh Kumar <108134761+shailesh7053@users.noreply.github.com>
2026-02-02 22:05:38 +0530

    Update ec2.tf

--- AWS-EC2/ec2.tf
+++ AWS-EC2/ec2.tf
@@ -2,11 +2,29 @@ resource "aws_instance" "windows" {
   ami                    = data.aws_ami.windows.id
   instance_type          = "t3.micro"
   subnet_id              = data.aws_subnets.public.ids[0]
-  vpc_security_group_ids = [aws_security_group.rdp_sg.id]
-
-  key_name = "windows-key-new"
+  vpc_security_group_ids = [data.aws_security_group.rdp_sg.id]
+  key_name               = "windows-key-new"
 
   associate_public_ip_address = true
+
+  user_data = <<-EOF
+<powershell>
+# Enable RDP
+Set-ItemProperty -Path "HKLM:\\System\\CurrentControlSet\\Control\\Terminal Server" `
+  -Name "fDenyTSConnections" -Value 0
+
+# Disable Network Level Authentication (NLA)
+Set-ItemProperty -Path "HKLM:\\System\\CurrentControlSet\\Control\\Terminal Server\\WinStations\\RDP-Tcp" `
+  -Name "UserAuthentication" -Value 0
+
+# Enable Windows Firewall rules for RDP
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+# Ensure RDP service is running
+Set-Service -Name TermService -StartupType Automatic
+Restart-Service -Name TermService
+</powershell>
+EOF
 
   tags = {
     Name = "windows-rdp-server"
