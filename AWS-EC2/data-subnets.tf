b9c400556e5ca03c87ed17656fae2aaafcdbf947
Shailesh Kumar <108134761+shailesh7053@users.noreply.github.com>
2026-02-02 20:31:08 +0530

    Update data-subnets.tf

--- AWS-EC2/data-subnets.tf
+++ AWS-EC2/data-subnets.tf
@@ -1,8 +1,9 @@
 data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.existing.id]
+    values = [data.terraform_remote_state.vpc.outputs.vpc_id]
   }
+}
 
   filter {
     name   = "tag:Name"
