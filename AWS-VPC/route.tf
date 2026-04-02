c9ab96369a76653e925daaa6ef5fbdd9127373e3
Shailesh Kumar <108134761+shailesh7053@users.noreply.github.com>
2026-04-02 20:39:56 +0530

    Delete private route table and NAT route configuration
    Removed private route table and associated NAT route.

--- AWS-VPC/route.tf
+++ AWS-VPC/route.tf
@@ -24,17 +24,3 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public_rt.id
 }
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "private-rt"
-  }
-}
-
-resource "aws_route" "private_nat" {
-  route_table_id         = aws_route_table.private_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
-}
